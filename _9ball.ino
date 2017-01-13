5222992e3610a78a1c2c6b4bb07f4f5300350110
SteelSaint <Archsaint@gmail.com>
2017-01-13 12:08:14 -0600

    Add files via upload
    This is the master branch of the project, this code is a combination of the elegoo team's bluetooth and obstacle avoidance.

---
+++ _9ball.ino
@@ -0,0 +1,243 @@
+//The beginning of 9ball
+//Seems that Arduino uses C++ like lanugage, keep this in mind.
+//Can chain things together, ie int a, b;
+//TODO: Figure out how to put bluetooth controls etc etc into their own function to call to in loop (or main).
+//Arduino Sensor Shield 5.0 needs to have the 11th pin (Supposedly ENA=11) to power the motors? 
+//Thanks to Elegoo.com for this first pass through of the system 
+
+
+#include <Servo.h> //Servo library
+Servo myservo;
+int LED=13,in1=9, in2=8, in3=7, in4=6, ENA=10, ENB=5, ABS=130, Echo = A4, Trig = A5, rightDistance = 0,leftDistance = 0,middleDistance = 0 ;
+volatile int state = LOW;
+char getstate;
+/*
+void _blueForward()
+{
+  digitalWrite(ENA,HIGH);
+  digitalWrite(ENB,HIGH);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+  Serial.println("Forward!");
+}
+void _blueBack()
+{
+  digitalWrite(ENA,HIGH);
+  digitalWrite(ENB,HIGH);
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+  Serial.println("BACK!");
+}
+void _blueleft()
+{
+  analogWrite(ENA,ABS);
+  analogWrite(ENB,ABS);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+  Serial.println("Left!");
+}
+void _blueright()
+{
+  analogWrite(ENA,ABS);
+  analogWrite(ENB,ABS);
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+  Serial.println("Right!"); 
+}
+void _blueStop()
+{
+  digitalWrite(ENA,LOW);
+  digitalWrite(ENB,LOW);
+  Serial.println("Stop!");
+}
+void bluestateChange()
+{
+  state = !state;
+  digitalWrite(LED, state);  
+}*/
+void _mForward()
+{
+ analogWrite(ENA,ABS);
+ analogWrite(ENB,ABS);
+ digitalWrite(in1,LOW);
+ digitalWrite(in2,HIGH);
+ digitalWrite(in3,LOW);
+ digitalWrite(in4,HIGH);
+ Serial.println("go forward!");
+}
+
+void _mBack()
+{
+ analogWrite(ENA,ABS);
+ analogWrite(ENB,ABS);
+ digitalWrite(in1,HIGH);
+ digitalWrite(in2,LOW);
+ digitalWrite(in3,HIGH);
+ digitalWrite(in4,LOW);
+ Serial.println("go back!");
+}
+
+void _mleft()
+{
+ analogWrite(ENA,ABS);
+ analogWrite(ENB,ABS);
+ digitalWrite(in1,LOW);
+ digitalWrite(in2,HIGH);
+ digitalWrite(in3,HIGH);
+ digitalWrite(in4,LOW);
+ Serial.println("go left!");
+}
+
+void _mright()
+{
+ analogWrite(ENA,ABS);
+ analogWrite(ENB,ABS);
+ digitalWrite(in1,HIGH);
+ digitalWrite(in2,LOW);
+ digitalWrite(in3,LOW);
+ digitalWrite(in4,HIGH);
+ Serial.println("go right!");
+} 
+void _mStop()
+{
+  digitalWrite(ENA,LOW);
+  digitalWrite(ENB,LOW);
+  Serial.println("Stop!");
+} 
+ /*Ultrasonic distance measurement Sub function*/
+int Distance_test()   
+{
+  digitalWrite(Trig, LOW);   
+  delayMicroseconds(2);
+  digitalWrite(Trig, HIGH);  
+  delayMicroseconds(20);
+  digitalWrite(Trig, LOW);   
+  float Fdistance = pulseIn(Echo, HIGH);  
+  Fdistance= Fdistance/58;       
+  return (int)Fdistance;
+}
+void setup() 
+{/*
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3,OUTPUT);
+  pinMode(in4,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+  _blueStop();*/
+//Below is the obstacle avoidance bit
+  myservo.attach(3);// attach servo on pin 3 to servo object
+  Serial.begin(9600);     
+  pinMode(Echo, INPUT);    
+  pinMode(Trig, OUTPUT);  
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3,OUTPUT);
+  pinMode(in4,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+  _mStop();
+}
+
+void loop() 
+{
+  /*
+  getstate=Serial.read();
+  if(getstate=='f')
+  {
+    _blueForward();
+  }
+  else if(getstate=='b')
+  {
+    _blueBack();
+    delay(200);
+  }
+  else if(getstate=='l')
+  {
+    _blueleft();
+    delay(200);
+  }
+  else if(getstate=='r')
+  {
+    _blueright();
+    delay(200);
+  }
+  else if(getstate=='s')
+  {
+     _blueStop();     
+  }
+  else if(getstate=='A')
+  {
+  bluestateChange();
+  }
+*/
+//This is the loop for obstacle avoid
+    myservo.write(90);//setservo position according to scaled value
+    delay(500); 
+    middleDistance = Distance_test();
+    #ifdef send
+    Serial.print("middleDistance=");
+    Serial.println(middleDistance);
+    #endif
+
+    if(middleDistance<=20)
+    {     
+      _mStop();
+      delay(500);                         
+      myservo.write(5);          
+      delay(1000);      
+      rightDistance = Distance_test();
+
+      #ifdef send
+      Serial.print("rightDistance=");
+      Serial.println(rightDistance);
+      #endif
+
+      delay(500);
+       myservo.write(90);              
+      delay(1000);                                                  
+      myservo.write(180);              
+      delay(1000); 
+      leftDistance = Distance_test();
+
+      #ifdef send
+      Serial.print("leftDistance=");
+      Serial.println(leftDistance);
+      #endif
+
+      delay(500);
+      myservo.write(90);              
+      delay(1000);
+      if(rightDistance>leftDistance)  
+      {
+        _mright();
+        delay(180);
+       }
+       else if(rightDistance<leftDistance)
+       {
+        _mleft();
+        delay(180);
+       }
+       else if((rightDistance<=20)||(leftDistance<=20))
+       {
+        _mBack();
+        delay(180);
+       }
+       else
+       {
+        _mForward();
+       }
+    }  
+    else
+        _mForward();
+}
